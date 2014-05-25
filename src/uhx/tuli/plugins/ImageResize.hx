96f84d290f33a5fdd3ce2de4436945782e50ca16
Skial Bainn <skial.bainn@gmail.com>
2014-05-25 14:48:29 +0100

    Add ImageResize.hx

---
+++ src/uhx/tuli/plugins/ImageResize.hx
@@ -0,0 +1,41 @@
+package uhx.tuli.plugins;
+
+import sys.io.File;
+import uhx.sys.Tuli;
+import byte.ByteData;
+import uhx.lexer.CssLexer;
+import uhx.lexer.CssParser;
+import neko.imagemagick.Imagick;
+
+using Detox;
+using StringTools;
+using haxe.io.Path;
+using sys.FileSystem;
+
+/**
+ * ...
+ * @author Skial Bainn
+ */
+class ImageResize {
+
+	public static function main() return ImageResize;
+
+	public function new(tuli:Class<Tuli>) {
+		untyped Tuli = tuli;
+		
+		Tuli.onExtension( 'css', handler, Before );
+	}
+	
+	public function handler(file:TuliFile, content:String):String {
+		var parser = new CssParser();
+		var tokens = parser.toTokens( ByteData.ofString( content ), 'ImageResize-css' );
+		var mediaQueries = tokens.filter( function(t) {
+			return t.token.match(Keyword(AtRule(_, _, _)));
+		} );
+		trace( tokens );
+		trace( mediaQueries );
+		
+		return content;
+	}
+	
+}
