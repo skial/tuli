a95fc330a90ba967fbae1b7b6ba98943e3a30a29
Skial Bainn <skial.bainn@gmail.com>
2014-08-06 14:11:51 +0100

    Add Directory.hx which creates a directory recursively.

---
+++ src/uhx/tuli/util/Directory.hx
@@ -0,0 +1,32 @@
+package uhx.tuli.util;
+
+using StringTools;
+using haxe.io.Path;
+using sys.FileSystem;
+
+/**
+ * ...
+ * @author Skial Bainn
+ */
+class Directory {
+
+	public static function create(path:String) {
+		if (!path.directory().addTrailingSlash().exists()) {
+			
+			var parts = path.directory().split('/');
+			var missing = [parts.pop()];
+			
+			while (!Path.join( parts ).normalize().exists()) missing.push( parts.pop() );
+			
+			missing.reverse();
+			
+			var directory = Path.join( parts );
+			for (part in missing) {
+				directory = '$directory/$part/'.normalize().replace(' ', '-');
+				if (!directory.exists()) FileSystem.createDirectory( directory );
+			}
+			
+		}
+	}
+	
+}
