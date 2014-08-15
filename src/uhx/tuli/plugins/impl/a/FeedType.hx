2a068724578bcd8ab55641ec6c00b7d183c271c4
Skial Bainn <skial.bainn@gmail.com>
2014-08-15 09:27:57 +0100

    Add missed committed files.

---
+++ src/uhx/tuli/plugins/impl/a/FeedType.hx
@@ -0,0 +1,14 @@
+package uhx.tuli.plugins.impl.a;
+
+/**
+ * ...
+ * @author Skial Bainn
+ */
+@:enum
+abstract FeedType(String) {
+	
+	var Full = 'full';
+	var Summary = 'summary';
+	var Link = 'link';
+	
+}
---
+++ src/uhx/tuli/plugins/impl/t/Feed.hx
@@ -0,0 +1,11 @@
+package uhx.tuli.plugins.impl.t;
+
+import uhx.tuli.plugins.impl.a.FeedType;
+
+/**
+ * ...
+ * @author Skial Bainn
+ */
+typedef Feed = {
+	var type:Null<FeedType>;
+}
