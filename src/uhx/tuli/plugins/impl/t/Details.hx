8b55a3611c030d3bc2f719f7f51e1a519b570566
Skial Bainn <skial.bainn@gmail.com>
2014-08-18 15:02:06 +0100

    Type information for extra individual file info. #10

---
+++ src/uhx/tuli/plugins/impl/t/Details.hx
@@ -0,0 +1,18 @@
+package uhx.tuli.plugins.impl.t;
+
+/**
+ * ...
+ * @author Skial Bainn
+ */
+typedef Details = {
+	var slug:Null<String>;
+	var title:Null<String>;
+	var summary:Null<String>;
+	var template:Null<String>;
+	
+	var tags:Array<String>;
+	var categories:Array<String>;
+	
+	var authors:Array<String>;
+	var contributors:Array<String>;
+}
