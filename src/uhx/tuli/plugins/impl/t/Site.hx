5ca7fe9518f2b6cf54a2f124aee0e864f6d61d9e
Skial Bainn <skial.bainn@gmail.com>
2014-08-20 09:23:05 +0100

    Add Site.hx for site specific values.

---
+++ src/uhx/tuli/plugins/impl/t/Site.hx
@@ -0,0 +1,12 @@
+package uhx.tuli.plugins.impl.t;
+
+/**
+ * @author Skial Bainn
+ */
+
+typedef Site = {
+	var title:Null<String>;
+	var domain:Null<String>;
+	var dateFormat:Null<String>;
+	var authors:Null<Array<String>>;
+}
