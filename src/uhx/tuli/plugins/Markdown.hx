c3fd72b9df9e7e694c31065631d4638e23f60530
Skial Bainn <skial.bainn@gmail.com>
2015-01-08 11:28:42 +0000

    Converting Markdown.hx to a single program which can take data through stdin or via a path and output to stdout or via a path, in both HTML or JSON. The JSON output includes all resource information as well as the HTML output.

--- src/uhx/tuli/plugins/Markdown.hx
+++ src/uhx/tuli/plugins/Markdown.hx
@@ -1,32 +1,49 @@
 package uhx.tuli.plugins;
 
 import dtx.Tools;
-import uhx.sys.Tuli;
+import haxe.io.Eof;
+import haxe.Json;
+import sys.io.File;
+import tjson.TJSON;
+import uhx.sys.Ioe;
 import byte.ByteData;
-import uhx.tuli.util.File;
-import uhx.tuli.util.Spawn;
+import uhx.sys.ExitCode;
 import uhx.lexer.MarkdownParser;
 
 using Detox;
 using StringTools;
 using haxe.io.Path;
 using sys.FileSystem;
-using uhx.tuli.util.File.Util;
+
+@:forward @:enum abstract OutputFormat(String) from String to String {
+	public var HTML = 'html';
+	public var JSON = 'json';
+}
 
 /**
  * ...
  * @author Skial Bainn
  */
-class Markdown {
+@:cmd
+@:usage( 'markdown [options]' )
+class Markdown extends Ioe implements Klas {
 	
-	public static function main() return Markdown;
-	private static var tuli:Tuli;
+	public static function main() {
+		var md = new Markdown( Sys.args() );
+	}
 	
-	private static var fileCache:Map<String, String>;
+	@alias('f')
+	public var format:OutputFormat = 'html';
+	
+	@alias('i')
+	public var input:String;
+	
+	@alias('o')
+	public var output:String;
 	
 	// I hate this, need to spend some time on UTF8 so I dont have to manually
 	// add international characters.
-	public static var characters = [
+	private static var characters = [
 	'ş' => '&#x015F;', '№' => '&#x2116;',
 	'ê' => '&ecirc;', 'ä' => '&auml;',
 	'é' => '&eacute;', 'ø' => '&oslash;',
@@ -34,101 +51,60 @@ class Markdown {
 	'“'=>'&ldquo;', '”'=>'&rdquo;',
 	'É' => '&Eacute;', 'õ' => '&otilde;'];
 
-	public function new(t:Tuli) {
-		tuli = t;
-		if (fileCache == null) fileCache = new Map();
-		
-		tuli.onExtension('md', handler, Before);
+	public function new(args:Array<String>) {
+		super();
+		handler();
 	}
 	
-	public function handler(file:File) {
-		// The output location to save the generated html.
-		var spawned = (file.path.replace( tuli.config.input, tuli.config.output ).withoutExtension() + '/index.html').normalize();
-		var output = spawned;
-		var skip = FileSystem.exists( output ) && file.modified.getTime() < FileSystem.stat( output ).mtime.getTime();
-		
-		if (!skip) {
-			for (key in characters.keys()) file.content = file.content.replace(key, characters.get(key));
-			
-			var parser = new MarkdownParser();
-			var tokens = parser.toTokens( ByteData.ofString( file.content ), file.path );
-			var resources = new Map<String, {url:String,title:String}>();
-			parser.filterResources( tokens, resources );
-			
-			if (file.extra.md == null) file.extra.md = { };
-			file.extra.md.resources = resources;
-			
-			var html = [for (token in tokens) parser.printHTML( token, resources )].join('');
-			
-			// Look for a template in the markdown `[_template]: /path/file.html`
-			var template = resources.exists('_template') ? resources.get('_template') : { url:'', title:'' };
-			var location = if (template.url == '') {
-				'${tuli.config.input}/templates/_template.html'.normalize();
-			} else {
-				(file.path.directory() + '/${template.url}').normalize();
-			}
-			
-			if (template.title == null || template.title == '') {
-				var token = tokens.filter(function(t) return switch (t) {
-					case Keyword(Header(_, _, _)): true;
-					case _: false;
-				})[0];
-				
-				if (token != null) {
-					template.title = switch (token) {
-						case Keyword(Header(_, _, t)): 
-							parser.printString( token );
-							
-						case _: 
-							'';
-					}
-				}
-			}
-			
-			var content = '';
-			var tuliFiles = tuli.config.files.filter( function(f) return [location].indexOf( f.path ) > -1 );
-			for (tuliFile in tuliFiles) tuliFile.ignore = true;
-			
-			if (!fileCache.exists( location )) {
-				// Grab the templates content.
-				if (tuli.config.files.exists( location )) {
-					content = tuli.config.files.get( location ).content;
-					fileCache.set( location, content );
-				} else {
-					var f = new File( location );
-					content = f.content;
-					tuli.config.files.push( f );
-					fileCache.set( location, content );
-				}
-			} else {
-				content = fileCache.get( location );
-			}
-			
-			var dom = content.parse();
-			
-			for (key in characters.keys()) html = html.replace( characters.get(key), key );
-			dom.find('content[select="markdown"]').replaceWith( null, dtx.Tools.parse( html ) );
-			content = dom.html();
-			/*if (file.path.toLowerCase().indexOf( 'one year of haxe' ) > -1) {
-				trace( dtx.Tools.parse( html ) );
-				trace( content );
-			}*/
-			if (file.spawned.indexOf( spawned ) == -1) {
-				file.spawned.push( spawned );
-				
-				var spawn = new Spawn( spawned, file.path );
-				spawn.content = content;
-				spawn.extra.md = {};
-				spawn.extra.md.resources = resources;
-				spawn.created = file.created;
-				spawn.modified = file.modified;
-				
-				tuli.config.spawn.push( spawn );
-			}
-			
+	private function handler() {
+		if (input != null) {
+			stdin = File.read( input );
 		}
 		
-		file.ignore = true;
+		if (output != null) {
+			stdout = File.write( output );
+		}
+		
+		var content = '';
+		try {
+			content += stdin.readUntil( eofChar );
+		} catch (e:Eof) { 
+			
+		} catch (e:Dynamic) { 
+			stderr.writeString( '$e' );
+		}
+		
+		for (key in characters.keys()) content = content.replace(key, characters.get(key));
+		
+		var parser = new MarkdownParser();
+		var tokens = parser.toTokens( ByteData.ofString( content ), 'markdown-cmd' );
+		var resources = new Map<String, {url:String,title:String}>();
+		parser.filterResources( tokens, resources );
+		
+		var result = '';
+		
+		switch (format.toLowerCase()) {
+			case HTML:
+				result = [for (token in tokens) parser.printHTML( token, resources )].join('');
+				for (key in characters.keys()) result = result.replace( characters.get( key ), key );
+				
+			case JSON:
+				var _output = {
+					resources: { },
+					html: [for (token in tokens) parser.printHTML( token, resources )].join(''),
+				};
+				
+				for (key in resources.keys()) Reflect.setField( _output.resources, key, resources.get( key ) );
+				result = TJSON.encode( _output, 'fancy' );
+				
+			case _:
+				
+		}
+		
+		stdout.writeString( result );
+		
+		exitCode = ExitCode.SUCCESS;
+		exit();
 	}
 	
 }
